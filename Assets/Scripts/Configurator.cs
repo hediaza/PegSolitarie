128f95eafaa1ce60b788393bf782a150dcee32f8
Hernan Diaz <hediaza@unal.edu.co>
2018-03-21 19:44:30 -0500

    Se define la clase "Configurator" quien representará las variables de configuración fijas en el juego

---
+++ Assets/Scripts/Configurator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PegSolitarie
+{
+	/// <summary>
+	/// Clase que representa las variables de configuración fijas del juego.
+	/// </summary>
+	public class Configurator
+	{
+		public Configurator() {
+		}
+	}
+}
+
