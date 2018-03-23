b7404347f1aeae241dd643384e3a6573a151c514
leandroagudelov <lagudelov@unal.edu.co>
2018-03-22 19:26:14 -0500

    Se adiciona la clase "Cord" Clase que representa la coordenada en el siguiente plano que relaciona una matriz de 7*7

--- Assets/Scripts/Coord.cs
+++ Assets/Scripts/Coord.cs
@@ -1,3 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+namespace PegSolitarie
+{
+	/// <summary>
+	/// Clase que representa la coordenada en el siguiente plano 
+	/// que relaciona una matriz de 7*7
+	/// 
+	///   0 1 2 3 4 5 6 => eje X
+	/// 0 . . . . . . . 
+	/// 1 . . . . . . .
+	/// 2 . . . . . . .
+	/// 3 . . . . . . .
+	/// 4 . . . . . . .
+	/// 5 . . . . . . .
+	/// 6 . . . . . . .
+	/// => eje Y
+	/// </summary>
+	public class Coord  
+	{
+		public int x;
+		public int y;
+
+		public Coord(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
