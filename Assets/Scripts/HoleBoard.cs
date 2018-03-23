03058781b214b6992ac0b7c32b14a27c22dbebd2
ucamiand <criverosl@unal.edu.co>
2018-03-23 10:53:36 -0500

    Clase que representa la instancia del hueco en el tablero

--- Assets/Scripts/HoleBoard.cs
+++ Assets/Scripts/HoleBoard.cs
@@ -1,3 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+namespace PegSolitarie
+{
+	/// <summary>
+	/// Clase que representa la instancia del hueco en el tablero
+	/// </summary>
+	public class HoleBoard : MonoBehaviour 
+	{
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
+}
