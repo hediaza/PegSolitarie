654dfdffdb86c58740c32aa276e6d77db26680b4
leandroagudelov <lagudelov@unal.edu.co>
2018-03-22 21:42:48 -0500

    Se crea la estructura de la clase   representa la instancia de la ficha en el juego

--- Assets/Scripts/Peg.cs
+++ Assets/Scripts/Peg.cs
@@ -2,3 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace PegSolitarie
+{
+	/// <summary>
+	/// Metodo que representa la instancia de la ficha en el juego
+	/// </summary>
+	public class Peg : MonoBehaviour 
+	{
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+
+
+	}
+}
