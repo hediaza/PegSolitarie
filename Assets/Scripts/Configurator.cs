2dbf7b4d3e649bd2d6dea75adbf0cda7e1b001e5
leandroagudelov <lagudelov@unal.edu.co>
2018-03-24 19:45:20 -0500

    Desarrollo de las coordenadas del tablero Europeo  "EUROPEAN_BOARD"

--- Assets/Scripts/Configurator.cs
+++ Assets/Scripts/Configurator.cs
@@ -82,6 +82,19 @@ namespace PegSolitarie
 						new Coord(5,4), new Coord(6,4), new Coord(2,5), new Coord(3,5), new Coord(4,5), 
 						new Coord(2,6), new Coord(3,6), new Coord(4,6)
 					}
+				},
+				{
+					"EUROPEAN_BOARD", 
+					new List<Coord> {
+						new Coord(2,0), new Coord(3,0), new Coord(4,0), new Coord(2,1), new Coord(3,1), 
+						new Coord(4,1), new Coord(0,2), new Coord(1,2), new Coord(2,2), new Coord(3,2), 
+						new Coord(4,2), new Coord(5,2), new Coord(6,2), new Coord(0,3), new Coord(1,3), 
+						new Coord(2,3), new Coord(3,3), new Coord(4,3), new Coord(5,3), new Coord(6,3), 
+						new Coord(0,4), new Coord(1,4), new Coord(2,4), new Coord(3,4), new Coord(4,4), 
+						new Coord(5,4), new Coord(6,4), new Coord(2,5), new Coord(3,5), new Coord(4,5), 
+						new Coord(2,6), new Coord(3,6), new Coord(4,6), new Coord(1,1), new Coord(5,1),
+						new Coord(1,5), new Coord(5,5)
+					}
 				}
 			};
 
